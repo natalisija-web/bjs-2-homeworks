145211ae736c921be930fbe328321e49d3a34571
Nemets Nataly <e.n.nemets@gmail.com>
2025-03-26 14:23:55 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+	if (discriminant > 0) {
+		let answerFirst = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let answerSecond = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(answerFirst);
+		arr.push(answerSecond);
+	} else if (discriminant === 0) {
+		let answer = -b / (2 * a);
+		arr.push(answer);
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let percentPerMonth = percent / 100 / 12;
+	let principalSum = amount - contribution;
+	let payment = principalSum * (percentPerMonth + percentPerMonth / (Math.pow(1 + percentPerMonth, countMonths) - 1));
+	let totalCreditSum = +(payment * countMonths).toFixed(2);
+	return totalCreditSum;
 }
