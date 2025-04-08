27a2a211ef458ffe59c48c04062abc1cdc319a19
Nemets Nataly <e.n.nemets@gmail.com>
2025-04-08 09:03:15 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(value) {
+	let parseFloat = Number.parseFloat(value);
+	if (!parseFloat) {
+		throw new Error('Невалидное значение');
+	}
+	return parseFloat;
+}
+
+function validateCount(value) {
+	try {
+		return parseCount(value)
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		if (a + b < c || a + c < b || b + c < a) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	get perimeter() {
+		return Number(this.a + this.b + this.c);
+	}
+
+	get area() {
+		let p = this.perimeter / 2;
+		return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		irregularTriangle = {
+			get perimeter() {
+				return 'Ошибка! Треугольник не существует';
+			},
+			get area() {
+				return 'Ошибка! Треугольник не существует';
+			}
+		};
+		return irregularTriangle;
+	};
+}
